618932cc2242ab63ed5a31500f9d9e2b6c064c73
Ignaz503 <lukasneuhold2@gmail.com>
2021-05-18 15:32:19 +0200

    adjacency matrix is generated, stored as csv first line is comment marked with # has min and max value of matrix

--- data_generator.py
+++ data_generator.py
@@ -2,7 +2,9 @@ from app_config import Config
 from simple_logging import Logger
 import subreddit
 from subreddit import Subreddit_Data
-import defines
+from defines import DATA_BASE_PATH, ADJACENCY_MAT
+from os import path
+import numpy as np
 
 def sub_pairs(subs:list[str]):
   return [(subs[i],subs[j]) for i in range(len(subs)) for j in range(i+1, len(subs))]
@@ -15,19 +17,24 @@ def define_index_dict(subs: list[str]):
     count +=1
   return idx_dict
 
-def update_adjacency_matrix(index_dict: dict[str,int], pair: tuple[str,str], adjacency_mat):
-  sub_one: Subreddit_Data = subreddit.load(defines.DATA_BASE_PATH,pair[0])
-  sub_two: Subreddit_Data = subreddit.load(defines.DATA_BASE_PATH,pair[1]) 
-  #loop over users, check if other has same user, if yes note in adjacency matrix, else continue
-  raise NotImplementedError()
+def update_adjacency_matrix(index_dict: dict[str,int], pair: tuple[str,str], adjacency_mat: np.ndarray):
+  sub_one: Subreddit_Data = subreddit.load(DATA_BASE_PATH,pair[0])
+  sub_two: Subreddit_Data = subreddit.load(DATA_BASE_PATH,pair[1]) 
+  idx_one = index_dict[pair[0]]
+  idx_two = index_dict[pair[0]]
+  intersection_size = len(sub_one.users.intersection(sub_two.users))
+  adjacency_mat[idx_one,idx_two] = intersection_size
+  adjacency_mat[idx_two,idx_one] = intersection_size
 
-def save(adjacency_mat):
-  #todo
-  raise NotImplementedError()
+def save(adjacency_mat: np.ndarray):
+  header ="{mi}, {ma}".format(mi= adjacency_mat.min(),ma=adjacency_mat.max())
+  np.savetxt(path.join(DATA_BASE_PATH,ADJACENCY_MAT),adjacency_mat,fmt="%i", delimiter=",", header=header)
 
 def run(config: Config, logger: Logger):
   index_dict = define_index_dict(config.subreddits_to_crawl)
-  adjacency_mat = {} #todo numpy
+  logger.log(index_dict)
+  size = len(config.subreddits_to_crawl)
+  adjacency_mat = np.zeros((size,size), dtype=np.int32)
   for pair in sub_pairs(config.subreddits_to_crawl):
     update_adjacency_matrix(index_dict,pair,adjacency_mat)
   save(adjacency_mat)
--- defines.py
+++ defines.py
@@ -2,6 +2,7 @@ CLIENT_ID="oLm5KqTNCR5qrw"
 CLIENT_SECRET="E-uXbSsa6JTNph_zp49vnbSLZpO0tg"
 USER_AGENT="python:TUG-CommentCrawler:v1.0.0 (by u/Ignaz503)"
 DATA_BASE_PATH = "data"
+ADJACENCY_MAT = "mat.csv"
 CRAWL_ARGS = ["-crawl","-c"]
 GENERATE_ARGS = ["-generate","-g"]
 ALL_ARGS = ["-all","-a", "-both"]
